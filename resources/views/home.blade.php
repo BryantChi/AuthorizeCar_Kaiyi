272a93b0c743c05d75d87e36583a6476fb3370ec
BryantChi <Bryantchi.work@gmail.com>
2023-08-02 16:31:19 +0800

    [MOD] 基本UI調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-
+<div class="container" style="height: 45vmin;">
+    <div class="row justify-content-center h-100">
+        <div class="col-md-6 text-center align-self-end">
+            <a href="{{ route('admin.detectionReports.create') }}" class="btn btn-outline-primary btn-lg">
+                開始新增
+            </a>
+        </div>
     </div>
 </div>
 @endsection
