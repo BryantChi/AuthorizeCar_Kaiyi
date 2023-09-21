fb3d7f1e7ca42bc77a8e8bda8372a7b44966199f
BryantChi <Bryantchi.work@gmail.com>
2023-09-21 11:55:39 +0800

    [MOD] 調整授權書印章大小v5

--- app/Services/WordServices.php
+++ app/Services/WordServices.php
@@ -319,7 +319,7 @@ class WordServices
         $templateProcessor->setValue('a_d', $date_d);
 
         $company = Company::first();
-        $templateProcessor->setImageValue('image_sign_com', ["path" => public_path('uploads/'.$company->com_seal), "width" => 150, "height" => '']);
+        $templateProcessor->setImageValue('image_sign_com', ["path" => public_path('uploads/'.$company->com_seal), "width" => 200, "height" => '']);
         // $templateProcessor->setImageValue('image_sign_com', ["path" => public_path('assets/img/sign_test_icon/sign_com.png'), "width" => 150, "height" => '']);
 
         $time = Carbon::now();
