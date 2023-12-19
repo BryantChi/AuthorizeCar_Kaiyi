b7522890fa2a99004ff7911e1a7d63f4186d0191
BryantChi <Bryantchi.work@gmail.com>
2023-12-19 22:12:00 +0800

    [MOD] 閒置超過30分鐘自動登出並提示

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -207,6 +207,54 @@
                 }
             })
         }
+
+        $(function () {
+            var idleTime = 0;
+
+            // 增加每分鐘的閒置時間
+            var idleInterval = setInterval(timerIncrement, 60000); // 1 分鐘
+
+            // 重置閒置計時器當有活動發生時
+            $(this).mousemove(function(e){
+                idleTime = 0;
+            });
+            $(this).keypress(function(e){
+                idleTime = 0;
+            });
+
+            function timerIncrement() {
+                idleTime = idleTime + 1;
+                if (idleTime > 30) { // 30 分鐘
+                    clearInterval(idleInterval);
+                    Swal.fire({
+                        title: '注意！',
+                        text: "閒置太久<已自動登出>！",
+                        icon: 'warning',
+                        showCancelButton: false,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: '確認',
+                        cancelButtonText: '取消',
+                        allowOutsideClick: false
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            // $.ajax({
+                            //     url: '/logout', // Laravel 登出路由
+                            //     type: 'POST',
+                            //     // 需要添加 CSRF Token
+                            //     success: function(result) {
+                            //         window.location.href = '/login'; // 重定向到登入頁面
+                            //     }
+                            // });
+                            $('form#logout-form').submit();
+                        }
+                    })
+
+                }
+            }
+        })
+
+
     </script>
 
 </body>
