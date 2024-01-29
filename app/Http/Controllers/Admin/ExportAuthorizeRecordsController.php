bdc6db89e5b98382008167d88718a1e8b80383fe
BryantChi <Bryantchi.work@gmail.com>
2024-01-29 14:54:39 +0800

    [MOD] 修正json_encode韓空白字符的問題

--- app/Http/Controllers/Admin/ExportAuthorizeRecordsController.php
+++ app/Http/Controllers/Admin/ExportAuthorizeRecordsController.php
@@ -77,10 +77,10 @@ class ExportAuthorizeRecordsController extends AppBaseController
                     return '<div class="form-group form-check"><input type="checkbox" name="records[]" class="form-check-input" style="width: 20px;height: 20px;" value="' . $record->id . '" id="' . $record->id . '" ></div>';
                 })
                 ->addColumn('action', function (ExportAuthorizeRecords $record) {
-                    $btn_copy = '<a href="javascript:void(0)" onclick=copy(\''. json_encode($record) .'\') class="btn btn-default btn-lg2">' .
+                    $btn_copy = '<a href="javascript:void(0)" onclick=copy(\''. str_replace(" ", "&nbsp;", json_encode($record, JSON_UNESCAPED_UNICODE)) .'\') class="btn btn-default btn-lg2">' .
                         '<i class="far fa-copy"></i>' .
                         '</a>';
-                    $btn_edit = '<a href="javascript:void(0)" onclick=edit(\''. json_encode($record) .'\') class="btn btn-default btn-lg2">' .
+                    $btn_edit = '<a href="javascript:void(0)" onclick=edit(\''. str_replace(" ", "&nbsp;", json_encode($record, JSON_UNESCAPED_UNICODE)) .'\') class="btn btn-default btn-lg2">' .
                         '<i class="far fa-edit"></i>' .
                         '</a>';
                     $btn_del = '<button type="button" class="btn btn-danger" onclick="return check(this)"><i class="far fa-trash-alt"></i></button>';
